4303b7f0b51ff98af706dc50bca9a20040ee4f9d
Gabriel Lima <gabriellimagomes.14@hotmail.com>
2024-03-09 18:04:16 -0300

    output com sg

--- src/mssql-rds.tf
+++ src/mssql-rds.tf
@@ -24,3 +24,7 @@ resource "aws_db_instance" "mssql-techlanches-instance" {
   }
 
 }
+
+output "security_group_id" {
+  value = tolist(aws_db_instance.mssql-techlanches-instance.vpc_security_group_ids)[0]
+}
